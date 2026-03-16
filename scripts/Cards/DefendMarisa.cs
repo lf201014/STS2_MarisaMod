fa73c1063f11b6da28c43d290949390f62a76e13
Flynn1014 <lf201014@gmail.com>
2026-03-17 00:42:58 +0800

    Update DefendMarisa.cs

--- scripts/Cards/DefendMarisa.cs
+++ scripts/Cards/DefendMarisa.cs
@@ -1,5 +1,7 @@
 using marisamod.Scripts.Cards.Abstract;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 
@@ -16,5 +18,10 @@ namespace MarisaMod.scripts.Cards
         protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { CardTag.Defend };
 
         protected override IEnumerable<DynamicVar> CanonicalVars => new List<DynamicVar> { new BlockVar(5m, ValueProp.Move) };
+
+        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+            await CreatureCmd.GainBlock(base.Owner.Creature, base.DynamicVars.Block, cardPlay);
+        }
     }
 }
